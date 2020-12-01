b1ab3e67cc2819ba41528d50a9109e30b72c2c03
mcbridz@gmail.com <mcbridz@gmail.com>
2020-12-01 15:59:07 -0800

    Implemented slideshow

--- portfolio/html_css/static/html_css/slideshow.js
+++ portfolio/html_css/static/html_css/slideshow.js
@@ -24,5 +24,5 @@ function showSlides(n) {
         dots[i].className = dots[i].className.replace(" active", "")
     }
     slides[slideIndex - 1].style.display = "block"
-    dots[slideIndex - 1].className += " active"
+    dots[Math.floor(slideIndex / 5)].className += " active"
 }
